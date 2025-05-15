56282470f61f4aeadf102f23ab9bc720964e064c
kasader <casada980@gmail.com>
2025-05-15 20:38:14 +0900

    First commit

---
+++ cmd/main.go
@@ -0,0 +1,15 @@
+package main
+
+type Grid struct {
+	x    uint16
+	y    uint16
+	grid [][]string
+}
+
+func NewGrid(x, y uint16) *Grid {
+	return &Grid{
+		x:    x,
+		y:    y,
+		grid: make([][]string, x),
+	}
+}
